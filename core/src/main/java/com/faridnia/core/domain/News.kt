f31203f3b7a478742f445e06fccc67497d85a033
Milad <Milad.faridnia@gmail.com>
2020-08-14 19:25:13 +0430

    feat: Remove unused import

--- core/src/main/java/com/faridnia/core/domain/News.kt
+++ core/src/main/java/com/faridnia/core/domain/News.kt
@@ -1,9 +1,6 @@
 package com.faridnia.core.domain
 
 
-import com.faridnia.core.domain.NewsItem
-import com.google.gson.annotations.SerializedName
-
 data class News(
     val href: String,
     val newsItems: List<NewsItem>,
--- core/src/main/java/com/faridnia/core/domain/NewsItem.kt
+++ core/src/main/java/com/faridnia/core/domain/NewsItem.kt
@@ -1,8 +1,6 @@
 package com.faridnia.core.domain
 
 
-import com.google.gson.annotations.SerializedName
-
 data class NewsItem(
     val href: String,
     val ingredients: String,
